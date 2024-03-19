fb5298090147be9133b2c8548b991fdeeddfef74
samuel-casasdray <samuelcasasdray@gmail.com>
2024-03-19 19:00:40 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Component/Header";
+import "./App.css"
+import profil from "./assets/profil.jpg";
+import {Card, CardBody, CardHeader, Carousel, Typography} from "@material-tailwind/react";
+import data from "./data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const realisation = data.realisation.map(item => (
+        <Card className="mt-6 my-6 w-96 hover:shadow-xl transition-shadow" key={item.name}>
+            <CardHeader color="white" className="relative border-[1px]">
+                {item.img.length === 1 ?
+                    <img src={require("./assets/" + item.img[0])} alt={item.name} /> :
+                    <Carousel>
+                        {item.img.map((img, i) => (
+                            <img src={require("./assets/" + img)} alt={item.name + "_" + i} key={item.name + "_" + i} />
+                        ))}
+                    </Carousel>
+                }
+            </CardHeader>
+            <CardBody>
+                <Typography variant="h5" color="blue-gray" className="mb-2">
+                    {item.name}
+                </Typography>
+                <Typography>
+                    {item.desc}
+                </Typography>
+            </CardBody>
+        </Card>
+    ))
+
+    return (
+        <div className="App">
+            <Header />
+            <div className={"max-w-full flex p-32"}>
+                <img src={profil} className="h-80 mr-20 rounded-lg" alt="profil"/>
+                <div className={"flex flex-col m-auto"}>
+                    <Typography variant="h2" className="mb-5">Qui suis-je ?</Typography>
+                    <Typography variant="lead">Moi, c'est Fany, étudiante en communication actuellement en 5ᵉ année de Bachelor à Sup’ de Com Montpellier 😀</Typography>
+                </div>
+            </div>
+            <div className="max-w-full grid p-32 flex-wrap justify-between gap-4 grid-cols-repeat items-center" >
+                {realisation}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {(tailwindConfig: object) => object} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
+
